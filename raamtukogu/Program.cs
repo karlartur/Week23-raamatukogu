259439cf5d3815fcc67277640bae79e3c60dc2b2
karlartur <89841872+karlartur@users.noreply.github.com>
2022-03-15 11:38:03 +0200

    Add files via upload

---
+++ raamtukogu/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace raamtukogu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta raamatunimi:");
+            string raamatunimi = Console.ReadLine();
+            
+            Console.WriteLine("Sisesta raamatu autor:");
+            string raamatuautor = Console.ReadLine();
+            
+
+            Console.WriteLine("Sisesta raamatu hind:");
+            double raamatuhind = Convert.ToInt32(Console.ReadLine());
+
+            if (raamatunimi.Length > 4)
+            {
+                Console.WriteLine("Pealkiri kehtib!");
+            }
+            else if (raamatunimi.Length < 4)
+            {
+                Console.WriteLine("Pealkiri ei kehti!");
+            }
+            else
+            {
+                Console.WriteLine("Pealkiri ei kehti!");
+            }
+            
+            if (raamatuhind > 0 )
+            {
+                Console.WriteLine("Hind kehtib!");
+            }
+            else if(raamatuhind < 0)
+            {
+                Console.WriteLine("Hind ei kehti!");
+            }
+            else
+            {
+                Console.WriteLine("Hind ei kehti!");
+            }
+            
+        }
+    }
+}
---
+++ raamtukogu/Raamatud.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace raamtukogu
+{
+    internal class Raamatud
+    {
+        string pealkiri;
+        string autor;
+        double hind;
+        public Raamatud(string _pealkiri, string _autor, string _hind)
+        {
+            pealkiri = _pealkiri;
+            autor = _autor;
+            hind = 0.0;
+            Console.WriteLine("Sisesta raamatunimi:");
+            Console.WriteLine("Sisesta raamatu autor:");
+            Console.WriteLine("Sisesta raamatu hind:");
+        }
+        public string Raamatupealkiri
+        {
+            get { return pealkiri; }
+        }
+        public string Raamatuautor
+        {
+            get { return autor; }
+        }
+        public double Raamatuhind
+        {
+            get { return hind; }
+        }
+
+    }
+    
+
+}
